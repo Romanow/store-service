904ff6736cf8b5747a2c417654498eb792072dcd
romanow <romanowalex@mail.ru>
2025-01-10 17:26:01 +0300

    Configure tracing

--- common-service/src/main/kotlin/ru/romanow/inst/services/common/config/TracingConfiguration.kt
+++ common-service/src/main/kotlin/ru/romanow/inst/services/common/config/TracingConfiguration.kt
@@ -1,14 +1,26 @@
 package ru.romanow.inst.services.common.config
 
-import io.micrometer.tracing.exporter.SpanExportingPredicate
+import io.micrometer.observation.Observation
+import io.micrometer.observation.ObservationPredicate
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.server.observation.ServerRequestObservationContext
 
 @Configuration
 @ConditionalOnProperty("management.tracing.enabled", havingValue = "true")
 class TracingConfiguration {
 
     @Bean
-    fun noActuator() = SpanExportingPredicate { it.tags["uri"] == null || !it.tags["uri"]!!.startsWith("/manage") }
+    fun noActuatorObservations() = ObservationPredicate { name: String, context: Observation.Context? ->
+        if (name == "http.server.requests" && context is ServerRequestObservationContext) {
+            !context.carrier.servletPath.startsWith("/manage")
+        } else {
+            true
+        }
+    }
+
+    @Bean
+    fun noSecurityObservations() =
+        ObservationPredicate { name: String, _: Observation.Context? -> !name.startsWith("spring.security.") }
 }
