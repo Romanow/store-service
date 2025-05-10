2d961700311a2839ebc279e9e56b5e8bb9dce3e6
romanow <romanowalex@mail.ru>
2025-05-10 17:00:41 +0300

    Configure gateway

--- gateway/src/main/kotlin/ru/romanow/services/gateway/web/OpenApiController.kt
+++ gateway/src/main/kotlin/ru/romanow/services/gateway/web/OpenApiController.kt
@@ -3,10 +3,8 @@
  */
 package ru.romanow.services.gateway.web
 
-import io.swagger.v3.oas.models.OpenAPI
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import org.yaml.snakeyaml.Yaml
 import reactor.core.publisher.Mono
 
 @RestController
@@ -14,8 +12,9 @@ class OpenApiController {
 
     @GetMapping("/api/v1/openapi")
     fun openApi(): Mono<String> {
-        val openApi = OpenAPI()
-        return Mono.just(Yaml().dump(openApi)) // TODO
+//        val aggregatorService = DefaultOpenApiAggregatorService()
+//        val openApi = aggregatorService.aggregateOpenApi()
+        return Mono.empty() // Mono.just(Yaml.pretty().writeValueAsString(openApi))
     }
 
 }
