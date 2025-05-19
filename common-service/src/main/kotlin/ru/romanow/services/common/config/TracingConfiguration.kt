2d0b8a993074be41ed6df0e1f0f239b3fe466b35
romanow <romanowalex@mail.ru>
2025-05-19 21:11:20 +0300

    Configure Tracing

--- common-service/src/main/kotlin/ru/romanow/services/common/config/TracingConfiguration.kt
+++ common-service/src/main/kotlin/ru/romanow/services/common/config/TracingConfiguration.kt
@@ -6,7 +6,6 @@ package ru.romanow.services.common.config
 import io.micrometer.observation.Observation
 import io.micrometer.observation.ObservationPredicate
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.boot.autoconfigure.web.ServerProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.server.observation.ServerRequestObservationContext
@@ -14,14 +13,12 @@ import org.springframework.security.config.observation.SecurityObservationSettin
 
 @Configuration
 @ConditionalOnProperty("management.tracing.enabled", havingValue = "true")
-class TracingConfiguration(
-    private val serverProperties: ServerProperties
-) {
+class TracingConfiguration {
 
     @Bean
     fun noActuatorObservations() = ObservationPredicate { name: String, context: Observation.Context? ->
         if (name == "http.server.requests" && context is ServerRequestObservationContext) {
-            !context.carrier.servletPath.startsWith("${serverProperties.servlet.contextPath}/manage")
+            !context.carrier.servletPath.startsWith("/manage")
         } else {
             true
         }
--- gateway/src/main/kotlin/ru/romanow/services/gateway/config/TracingConfiguration.kt
+++ gateway/src/main/kotlin/ru/romanow/services/gateway/config/TracingConfiguration.kt
@@ -6,21 +6,19 @@ package ru.romanow.services.gateway.config
 import io.micrometer.observation.Observation
 import io.micrometer.observation.ObservationPredicate
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.boot.autoconfigure.web.ServerProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.server.observation.ServerRequestObservationContext
+import org.springframework.http.server.reactive.observation.ServerRequestObservationContext
 import org.springframework.security.config.observation.SecurityObservationSettings
-
 
 @Configuration
 @ConditionalOnProperty("management.tracing.enabled", havingValue = "true")
-class TracingConfiguration(private val serverProperties: ServerProperties) {
+class TracingConfiguration {
 
     @Bean
     fun noActuatorObservations() = ObservationPredicate { name: String, context: Observation.Context? ->
         if (name == "http.server.requests" && context is ServerRequestObservationContext) {
-            true // TODO
+            !context.carrier.path.pathWithinApplication().value().startsWith("/manage")
         } else {
             true
         }
